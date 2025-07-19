9cb35bd398e9cc3287e8dca85b5f7527eba26970
arvydasbussiness <arvydasbussiness@gmail.com>
2025-07-19 15:25:37 +0300

    test log

--- src/app/api/lambda/render/route.ts
+++ src/app/api/lambda/render/route.ts
@@ -12,20 +12,16 @@ import {
 } from "../../../../../config.mjs";
 import { RenderRequest } from "../../../../../types/schema";
 import { executeApi } from "../../../../helpers/api-response";
+import { stringify } from "querystring";
 
 export const POST = executeApi<RenderMediaOnLambdaOutput, typeof RenderRequest>(
   RenderRequest,
   async (req, body) => {
-    console.log('Environment check:', {
-  hasAWS: !!process.env.AWS_ACCESS_KEY_ID,
-  hasREMOTION: !!process.env.REMOTION_AWS_ACCESS_KEY_ID,
-  hasAWSSecret: !!process.env.AWS_SECRET_ACCESS_KEY,
-  hasREMOTIONSecret: !!process.env.REMOTION_AWS_SECRET_ACCESS_KEY
-});
     if (
       !process.env.AWS_ACCESS_KEY_ID &&
       !process.env.REMOTION_AWS_ACCESS_KEY_ID
     ) {
+       throw new TypeError("asdsad: " + process.env.REMOTION_AWS_ACCESS_KEY_ID);
       throw new TypeError(
         "Set up Remotion Lambda to render videos. See the README.md for how to do so.",
       );
